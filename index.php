bf8adaf1cfba31f13ca188ff01bc286ca7f0cd13
bjavier9 <31942457+bjavier9@users.noreply.github.com>
2019-07-02 01:58:07 +0000

    Add files via upload

--- index.php
+++ index.php
@@ -1,15 +1,33 @@
 <?php
+
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
 
 
 require_once "vendor/autoload.php";
-$app = new \Slim\App();
+$configuration = [
+    'settings' => [
+        'displayErrorDetails' => true,
+    ],
+];
+$configuration = new \Slim\Container($configuration);
+$app = new \Slim\App($configuration);
+$mid01 = function (Request $request, Response $response, $next): Response {
+    $response->getBody()->write("Dentro Do middleware01 ");
+    $next($request, $response);
+    $response->getBody()->write("Dentro Do middleware02 ");
+    return $response;
+};
+$app->group('/v1',function() use($app){
+    $app->get('/',function (Request $request, Response $response, array $args){});
+    $app->get('/test',function (Request $request, Response $response, array $args){});
+})->add($mid01);
+$app->post('/test', function (Request $request, Response $response, array $args): Response {
+    $data = $request->getParsedBody();
+    $nombre = $data['nombre'] ?? '';
+    $response->getBody()->write("Producto {$nombre} (POST)");
+    return $response;
+})->add($mid01);
 
-$app->get('/test[/{name}]',function(Request $request,Response $response, Array $args) {
-    $limit = $request->getQueryParams()['limit']??10;
-    $name = $args['name']??'jajajaja';
-    return $response->getBody()->write("{$limit} hellow world {$name}");
-});
 
 $app->run();
